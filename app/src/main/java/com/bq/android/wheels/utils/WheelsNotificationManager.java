b3f511f9ec7807338e1deb49c286d6ef439e9465
LiuBen <liuben1204@126.com>
2017-01-14 12:19:35 +0800

    home_modify notification manager
    Signed-off-by: LiuBen <liuben1204@126.com>

--- app/src/main/java/com/bq/android/wheels/utils/WheelsNotificationManager.java
+++ app/src/main/java/com/bq/android/wheels/utils/WheelsNotificationManager.java
@@ -1,82 +1,82 @@
-//package com.bq.android.wheels.utils;
-//
-//import android.app.Activity;
-//import android.app.NotificationManager;
-//import android.app.PendingIntent;
-//import android.app.TaskStackBuilder;
-//import android.content.Context;
-//import android.content.Intent;
-//import android.support.v7.app.NotificationCompat;
-//import android.widget.RemoteViews;
-//
-//import com.bq.android.wheels.R;
-//import com.bq.android.wheels.ui.MainActivity;
-//
-///**
-// * Created by liuben on 16-12-16.
-// */
-//
-//public class WheelsNotificationManager {
-//
-//    private static volatile WheelsNotificationManager mTestNotification = null;
-//    private Context mContext;
-//    private NotificationManager mNotificationManager;
-//
-//    private WheelsNotificationManager(Context context) {
-//        mContext = context.getApplicationContext();
-//        mNotificationManager = (NotificationManager)
-//                mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-//    }
-//
-//    public static WheelsNotificationManager getInstance(Context context) {
-//        if (mTestNotification == null) {
-//            synchronized (WheelsNotificationManager.class) {
-//                if (mTestNotification == null) {
-//                    mTestNotification = new WheelsNotificationManager(context);
-//                }
-//            }
-//        }
-//        return mTestNotification;
-//    }
-//
-//    /**
-//     * 不带TaskStackBuilder，正常通过notification启动一个Activity
-//     */
-//    public void sendNormalNotification(int id, int icon, String title, String content,
-//                                       Intent intent, boolean isClickGone) {
-//        PendingIntent pendingIntent = PendingIntent.
-//                getActivity(mContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
-//        builder.setSmallIcon(icon)
-//                .setContentTitle(title)
-//                .setContentText(content)
-//                .setContentIntent(pendingIntent)
-//                .setAutoCancel(isClickGone);
-//        mNotificationManager.notify(id, builder.build());
-//    }
-//
-//    /**
-//     * 通过TaskStackBuilder，实现跳转Activity后按back键不直接返回HOME
-//     * 而是返回指定的parentActivity（需要在manifest中注册）
-//     */
-//    public void sendSpecialNotification(int id, int icon, String title, String content,
-//                                        Class<? extends Activity> activityClass, boolean isClickGone) {
-//        Intent intent = new Intent(mContext, activityClass);
-//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(mContext);
-//        stackBuilder.addParentStack(activityClass);
-//        stackBuilder.addNextIntent(intent);
-//        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0,
-//                PendingIntent.FLAG_UPDATE_CURRENT);
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
-//        builder.setSmallIcon(icon)
-//                .setContentTitle(title)
-//                .setContentText(content)
-//                .setContentIntent(pendingIntent)
-//                .setAutoCancel(isClickGone);
-//        mNotificationManager.notify(id, builder.build());
-//    }
-//
-//
+package com.bq.android.wheels.utils;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+import android.widget.RemoteViews;
+
+import com.bq.android.wheels.R;
+import com.bq.android.wheels.ui.MainActivity;
+
+/**
+ * Created by liuben on 16-12-16.
+ */
+
+public class WheelsNotificationManager {
+
+    private static volatile WheelsNotificationManager mTestNotification = null;
+    private Context mContext;
+    private NotificationManager mNotificationManager;
+
+    private WheelsNotificationManager(Context context) {
+        mContext = context.getApplicationContext();
+        mNotificationManager = (NotificationManager)
+                mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    public static WheelsNotificationManager getInstance(Context context) {
+        if (mTestNotification == null) {
+            synchronized (WheelsNotificationManager.class) {
+                if (mTestNotification == null) {
+                    mTestNotification = new WheelsNotificationManager(context);
+                }
+            }
+        }
+        return mTestNotification;
+    }
+
+    /**
+     * 不带TaskStackBuilder，正常通过notification启动一个Activity
+     */
+    public void sendNormalNotification(int id, int icon, String title, String content,
+                                       Intent intent, boolean isClickGone) {
+        PendingIntent pendingIntent = PendingIntent.
+                getActivity(mContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+        builder.setSmallIcon(icon)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(isClickGone);
+        mNotificationManager.notify(id, builder.build());
+    }
+
+    /**
+     * 通过TaskStackBuilder，实现跳转Activity后按back键不直接返回HOME
+     * 而是返回指定的parentActivity（需要在manifest中注册）
+     */
+    public void sendSpecialNotification(int id, int icon, String title, String content,
+                                        Class<? extends Activity> activityClass, boolean isClickGone) {
+        Intent intent = new Intent(mContext, activityClass);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(mContext);
+        stackBuilder.addParentStack(activityClass);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+        builder.setSmallIcon(icon)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(isClickGone);
+        mNotificationManager.notify(id, builder.build());
+    }
+
+
 //    public void sendCustomNotification() {
 //        RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.notification_remote_views);
 //        remoteViews.setImageViewResource(R.id.icon, R.drawable.icon_test_app);
@@ -97,13 +97,13 @@
 //                .setAutoCancel(true);
 //        mNotificationManager.notify(0, builder.build());
 //    }
-//
-//    public void cancelNotification(int id) {
-//        mNotificationManager.cancel(id);
-//    }
-//
-//    public void cancelNotification(String tag, int id) {
-//        mNotificationManager.cancel(tag, id);
-//    }
-//
-//}
+
+    public void cancelNotification(int id) {
+        mNotificationManager.cancel(id);
+    }
+
+    public void cancelNotification(String tag, int id) {
+        mNotificationManager.cancel(tag, id);
+    }
+
+}
